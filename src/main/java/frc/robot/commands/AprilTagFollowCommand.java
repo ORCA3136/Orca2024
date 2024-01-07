d971dcea7770dea7651ceec530b52d731b50da46
orca3136driver <61524942+orca3136driver@users.noreply.github.com>
2024-01-07 15:42:26 -0500

    inital commit

---
+++ src/main/java/frc/robot/commands/AprilTagFollowCommand.java
@@ -0,0 +1,73 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSubsystem;
+
+import frc.robot.LimelightHelpers;
+
+public class AprilTagFollowCommand extends Command {
+  /** Creates a new AprilTagFollowCommand. */
+
+  DriveSubsystem m_robotDrive;
+  String limelightName = "limelight";
+
+  public AprilTagFollowCommand(DriveSubsystem robotDrive) {
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    m_robotDrive = robotDrive;
+
+    addRequirements(robotDrive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    //Positive: Target is right of cursor
+    double m_tx = LimelightHelpers.getTX(limelightName);
+    //Positive: Target is above cursor
+    double m_ty = LimelightHelpers.getTY(limelightName);
+    //Bigger number means closer
+    double m_ta = LimelightHelpers.getTA(limelightName);
+
+    double xSpeed = 0.0;
+    if (m_ta > 0.1 && m_ta < 4) {
+      xSpeed = 0.2;
+    }
+
+    double rot = 0;
+    if (m_tx < -3) {
+      rot = 0.03;
+    } else if (m_tx > 3) {
+      rot = -0.03;
+    }
+
+
+    m_robotDrive.drive(xSpeed, 0, rot, false, true);
+
+     /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param xSpeed        Speed of the robot in the x direction (forward).
+   * @param ySpeed        Speed of the robot in the y direction (sideways).
+   * @param rot           Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the
+   *                      field.
+   * @param rateLimit     Whether to enable rate limiting for smoother control.
+   */
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
